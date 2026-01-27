c850b32d69c6336314bf087dd47c91e0efb45843
Bil Bas <bil.bagpuss@gmail.com>
2026-01-27 17:24:41 +0000

    Bare bones kivy app

---
+++ lamm-lot.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class LAMMLoTWindow(Widget):
+    pass
+
+
+class LAMMLoTApp(App):
+    def build(self):
+        return LAMMLoTWindow()
+  
+
+if __name__ == "__main__":
+   LAMMLotApp.run()

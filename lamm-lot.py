0eb5a02d2af2ed373193e676ea73f2d267069bec
Bil Bas <bil.bagpuss@gmail.com>
2026-02-01 23:51:55 +0000

    getting less awful layout

--- lamm-lot.py
+++ lamm-lot.py
@@ -5,7 +5,7 @@ from kivymd.app import MDApp
 from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.uix.dropdownitem import MDDropDownItem
 from kivymd.uix.menu import MDDropdownMenu
-from kivymd.uix.widget import MDWidget
+from kivymd.uix.label import MDLabel
 from kivymd.toast import toast
 
 
@@ -13,29 +13,52 @@ class LAMMLotWindow(MDGridLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self._locations: list[dict] = []
-        self.ids.location_picker.text = "Lancaster Community Makerspace"
+        self._locations: list[str] = []
+        self._item_data: list[dict] = []
 
-    def refresh_locations(self, widget: MDWidget = None):
-        self._locations = [
+        self.refresh_locations()
+        self.refresh_items()
+
+        self.ids.location_picker.text = self._locations[0]  # TODO: read from settings file.
+
+    def refresh_locations(self):
+        self._locations = ["Lancaster Community Makerspace", "Lancaster BID", "Good Things Collective"]
+
+    def refresh_items(self):
+        self._items = [
             {
-                "text": location,
-                "on_release": lambda x=location: self.callback_location_menu(x),
-            } for location in ["Lancaster Community Makerspace", "Lancaster BID", "Good Things Collective"]
+                "text": "fish",
+
+            },
+            {
+                "text": "cheese",
+            }
         ]
 
-    def open_location_menu(self, item: MDDropDownItem):
+        items = self.ids.item_list
+
+        items.clear_widgets()
+
+        for item in self._items:
+            items.add_widget(MDLabel(text=item["text"]))
+
+    def location_data(self):
         if not self._locations:
             self.refresh_locations()
 
-        # This should be left-aligned from the button, but no idea how to do that right now!
-        self._location_menu = MDDropdownMenu(caller=item, items=self._locations,
-                                             pos_hint={"center_x": 0.5})
+        return [
+            {
+                "text": f"{location}",
+                "on_release": lambda x=f"{location}": self.callback_location_menu(x),
+            } for location in self._locations
+        ]
+
+    def open_location_menu(self, item: MDDropDownItem):
+        self._location_menu = MDDropdownMenu(caller=item, items=self.location_data(), width=500)
         self._location_menu.open()
 
     def callback_location_menu(self, text_item: str):
         self._location_menu.dismiss()
-        toast(f"Changed location to {text_item}")
         self.ids.location_picker.text = text_item
 
 

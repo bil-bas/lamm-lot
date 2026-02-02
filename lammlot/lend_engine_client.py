fa9b6df555aa0d15a7af1e694480c673a5f5a5ab
Bil Bas <bil.bagpuss@gmail.com>
2026-02-02 18:09:37 +0000

    requesting data from the API

--- lammlot/lend_engine_client.py
+++ lammlot/lend_engine_client.py
@@ -1,12 +1,27 @@
-class LendEngineClient:
-    def fetch_locations(self) -> list[str]:
-        return ["Lancaster Community Makerspace", "Lancaster BID", "Good Things Collective"]
+import os
+import requests
 
-    def fetch_items(self, query: str) -> list[dict]:
-        return [
-            {
-                "title": f"{query} {i + 1}",
-                "description": "Something, something, something, something, something",
-                "cost": "2",
-            } for i in range(20)
-        ]
+class LendEngineClient:
+    BASE_URL = "https://lammlibrary.lend-engine-app.com/api/2/"
+    ENV_TOKEN = "LEND_ENGINE_TOKEN"
+
+    def __init__(self):
+        response = requests.post(f"{self.BASE_URL}token/refresh",
+                                 data={"refresh_token": os.environ[self.ENV_TOKEN]},
+                                 verify=False)
+        self._token = response.json()["token"]
+
+    def _get_list(self, uri, **kwargs):
+        response = requests.get(f"{self.BASE_URL}{uri}",
+                                headers={"Authorization": f"Bearer {self._token}"},
+                                verify=False,
+                                **kwargs)
+        return response.json()
+
+    def fetch_sites(self) -> list[str]:
+        sites = self._get_list("sites")["hydra:member"]
+        return [site for site in sites if site["isActive"]]
+
+    def fetch_items(self, site: str, query: str) -> list[dict]:
+        items = self._get_list("items", params={})["hydra:member"]
+        return [item for item in items if item["isActive"] and item["itemType"] == "loan" and site in item["sites"]]
--- lammlot/main_window.py
+++ lammlot/main_window.py
@@ -11,20 +11,22 @@ class MainWindow(MDBoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self._locations: list[str] = []
+        self._sites: list[str] = []
         self._item_data: list[dict] = []
 
         self._client = LendEngineClient()
-        self.refresh_locations()
+
+        self.refresh_sites()
+        self._current_site = self._sites[0]  # TODO: read from settings file.
+        self.ids.site_picker.text = self._current_site["name"]
+
         self.refresh_items()
 
-        self.ids.location_picker.text = self._locations[0]  # TODO: read from settings file.
-
-    def refresh_locations(self):
-        self._locations = self._client.fetch_locations()
+    def refresh_sites(self):
+        self._sites = self._client.fetch_sites()
 
     def refresh_items(self):
-        items = self._client.fetch_items(self.ids.search.text or "search")
+        items = self._client.fetch_items(site=self._current_site["@id"], query=self.ids.search.text)
 
         items_list = self.ids.item_list
 
@@ -33,27 +35,30 @@ class MainWindow(MDBoxLayout):
         for item in items:
             items_list.add_widget(
                 ThreeLineIconListItem(
-                    text=item["title"],
-                    secondary_text=item["description"],
-                    tertiary_text=f"£{item["cost"]} per week",
+                    text=f"{item["sku"]} - {item["name"]["en"]}",
+                    secondary_text=item["description"]["en"] or "",
+                    tertiary_text=f"£{item["loanFee"]} per week",
                 )
             )
 
-    def location_data(self):
-        if not self._locations:
-            self.refresh_locations()
+    def site_data(self):
+        if not self._sites:
+            self.refresh_sites()
 
         return [
             {
-                "text": f"{location}",
-                "on_release": lambda x=f"{location}": self.callback_location_menu(x),
-            } for location in self._locations
+                "text": f"{site["name"]}",
+                "on_release": lambda x=f"{i}": self.callback_site_menu(x),
+            } for i, site in enumerate(self._sites)
         ]
 
-    def open_location_menu(self, item: MDDropDownItem):
-        self._location_menu = MDDropdownMenu(caller=item, items=self.location_data(), width=500)
-        self._location_menu.open()
+    def open_site_menu(self, item: MDDropDownItem):
+        self._site_menu = MDDropdownMenu(caller=item, items=self.site_data(), width=500)
+        self._site_menu.open()
 
-    def callback_location_menu(self, text_item: str):
-        self._location_menu.dismiss()
-        self.ids.location_picker.text = text_item
+    def callback_site_menu(self, item_index: str):
+        self._site_menu.dismiss()
+        self._current_site = self._sites[int(item_index)]
+        self.ids.site_picker.text = self._current_site["name"]
+
+        self.refresh_items()

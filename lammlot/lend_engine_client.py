bf9e419d12b851de1760ade4ea844ebec51ab1b4
Bil Bas <bil.bagpuss@gmail.com>
2026-02-02 21:46:04 +0000

    better url construction

--- lammlot/lend_engine_client.py
+++ lammlot/lend_engine_client.py
@@ -2,11 +2,11 @@ import os
 import requests
 
 class LendEngineClient:
-    BASE_URL = "https://lammlibrary.lend-engine-app.com/api/2/"
+    BASE_URL = "https://lammlibrary.lend-engine-app.com/api/2"
     ENV_TOKEN = "LEND_ENGINE_TOKEN"
 
     def __init__(self):
-        response = requests.post(f"{self.BASE_URL}token/refresh",
+        response = requests.post(f"{self.BASE_URL}/token/refresh",
                                  data={"refresh_token": os.environ[self.ENV_TOKEN]},
                                  verify=False)
         self._token = response.json()["token"]
@@ -19,9 +19,9 @@ class LendEngineClient:
         return response.json()
 
     def fetch_sites(self) -> list[str]:
-        sites = self._get_list("sites")["hydra:member"]
+        sites = self._get_list("/sites")["hydra:member"]
         return [site for site in sites if site["isActive"]]
 
     def fetch_items(self, site: str, query: str) -> list[dict]:
-        items = self._get_list("items", params={})["hydra:member"]
+        items = self._get_list("/items", params={})["hydra:member"]
         return [item for item in items if item["isActive"] and item["itemType"] == "loan" and site in item["sites"]]
